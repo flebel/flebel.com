586088fe3a207331d6cc86c5db06e54b889d15b4
Francois Lebel <francoislebel@gmail.com>
2013-06-01 20:31:46 -0400

    Initial import.

---
+++ Gruntfile.js
@@ -0,0 +1,31 @@
+module.exports = function(grunt) {
+
+  grunt.initConfig({
+    pkg: grunt.file.readJSON('package.json'),
+    clean: {
+      release: 'release',
+      'release-media': ['release/media/js/*.js', '!release/media/js/app.js']
+    },
+    copy: {
+      release: {
+        files: {
+          'release/': ['index.html', 'media/**']
+        }
+      }
+    },
+    useminPrepare: {
+      html: 'release/index.html'
+    },
+    usemin: {
+      html: 'release/index.html'
+    }
+  });
+
+  grunt.loadNpmTasks('grunt-compass');
+  grunt.loadNpmTasks('grunt-contrib');
+  grunt.loadNpmTasks('grunt-contrib-uglify');
+  grunt.loadNpmTasks('grunt-usemin');
+
+  grunt.registerTask('default', ['clean:release', 'copy', 'useminPrepare', 'concat', 'uglify', 'usemin', 'clean:release-media']);
+};
+
