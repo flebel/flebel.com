b4b8734bd9875484f3825fef5602f092bf38f086
Francois Lebel <francoislebel@gmail.com>
2013-06-02 12:46:19 -0400

    Added cssmin task.

--- Gruntfile.js
+++ Gruntfile.js
@@ -4,7 +4,8 @@ module.exports = function(grunt) {
     pkg: grunt.file.readJSON('package.json'),
     clean: {
       release: 'release',
-      'release-media': ['release/media/js/*.js', '!release/media/js/app.js']
+      'release-media': ['release/media/css/*.css', '!release/media/css/app.css',
+                        'release/media/js/*.js', '!release/media/js/app.js']
     },
     copy: {
       release: {
@@ -26,6 +27,6 @@ module.exports = function(grunt) {
   grunt.loadNpmTasks('grunt-contrib-uglify');
   grunt.loadNpmTasks('grunt-usemin');
 
-  grunt.registerTask('default', ['clean:release', 'copy', 'useminPrepare', 'concat', 'uglify', 'usemin', 'clean:release-media']);
+  grunt.registerTask('default', ['clean:release', 'copy', 'useminPrepare', 'concat', 'cssmin', 'uglify', 'usemin', 'clean:release-media']);
 };
 
